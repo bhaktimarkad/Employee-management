7dbf7e6d2b7c097e343530c2c7e8ee84964f82e4
BHAKTI <bhaktimarkad123@gmail.com>
2022-05-07 15:32:58 +0530

    first commit

---
+++ emp_app/views.py
@@ -0,0 +1,91 @@
+from datetime import datetime
+from urllib import request
+
+from django.shortcuts import render ,  HttpResponse
+from .models import Employee,Role,Department
+from django.db.models import Q
+# Create your views here.
+def index(request):
+
+    return render(request,'index.html')
+
+def all_emp(request):
+    emps = Employee.objects.all()
+    context = {
+        'emps': emps
+    }
+    print(context)
+    return render(request, 'all_emp.html', context)
+
+
+
+def add_emp(request):
+    if request.method == 'POST':
+
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        salary=int(request.POST['salary'])
+        Bonus=int(request.POST['Bonus'])
+        phone=int(request.POST['phone'])
+        dept=request.POST['dept']
+
+        Role = int(request.POST['Role'])
+        new_emp = Employee(first_name=first_name,last_name=last_name,salary=salary,bonus=Bonus,phone=phone,dept_id=dept,role_id=Role,hire_date=datetime.now())
+        new_emp.save()
+        #return HttpResponse('Employee added succesfully')
+        return render(request, 'index.html')
+
+
+    elif request.method=='GET':
+        return render(request,'add_emp.html')
+    else:
+        return HttpResponse("Exception Occured! Employee")
+
+
+
+
+def remove_emp(request, emp_id=0):
+    print("emp_id=",emp_id)
+    print("request",request)
+    if emp_id:
+        try:
+            emp_to_be_removed = Employee.objects.get(id=emp_id)
+            emp_to_be_removed.delete()
+            return HttpResponse("Employee Removed successfully")
+        except:
+            return HttpResponse("Please Enter A Valid Emp ID")
+    emps = Employee.objects.all()
+    context = {
+        'emps': emps
+    }
+
+    return render(request, 'remove_emp.html', context)
+
+
+
+def filter_emp(request):
+    print("filter")
+    if request.method=='POST':
+        print("request.POST=",request.POST)
+        name = request.POST['name']
+        dept = request.POST['dept']
+        role = request.POST['role']
+        emps = Employee.objects.all()
+        if name:
+            print("in name")
+            emps = emps.filter(Q(first_name__icontains = name)| Q (last_name__icontains = name))
+            print("emps=",emps)
+        if dept:
+            emps = emps.filter(dept__name__icontains = dept)
+        if role:
+            emps = emps.filter(role__name__icontains = role)
+        context = {
+                'emps': emps
+            }
+        print("return context")
+        return render(request,'all_emp.html',context)
+
+    elif request.method=="GET":
+        return render(request,'filter_emp.html')
+    else:
+        return HttpResponse('An exception occure')
